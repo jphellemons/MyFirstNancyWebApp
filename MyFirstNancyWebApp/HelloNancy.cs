8aa75f636d7254d598a72357569cf5e32b6642d2
jphellemons <info@jphellemons.nl>
2012-09-13 16:27:29 +0200

    first commit
    My first commit of my very first nancy webapplication

---
+++ MyFirstNancyWebApp/HelloNancy.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+
+namespace MyFirstNancyWebApp
+{
+    public class HelloNancy: NancyModule
+    {
+        public HelloNancy()
+        {
+            Get["/"] = parameters => "Hello World";
+            Get["/hello/{name}"] = parameters => "Hello " + parameters.name;
+
+            Get["/products"] = parameters => View["products.html", new ProductsModel(){Code="C1009", Name ="Product name here"}];
+        }    
+    }
+
+    public class ProductsModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
